27eb5c7f555c9aca60ba527fe31c4e0ad37498a3
Amapo057 <jiseong2002@naver.com>
2024-11-29 16:55:54 +0900

    update event

--- event.py
+++ event.py
@@ -18,7 +18,7 @@ for i, path in enumerate(cg_path):
 # 폰트 확인
 # for i in pg.font.get_fonts():
 #     print(i)
-font = pg.font.SysFont('malgungothic', 20, True, False)  # None은 기본 폰트, 폰트 크기
+font = pg.font.SysFont('malgungothic', 16, True, False)  # None은 기본 폰트, 폰트 크기
 color1 = (0, 0, 0)
 
 # 변수들
@@ -26,15 +26,16 @@ day = 1
 step = 0
 choice = 0
 choice2 = 0
+pers = 0
+perb = False
 
-m1_e1 = ["트럭이 당신을 향해 달려온다. 어떻게 해야할까 ", 
-       "1. 피한다", 
-       "2. 포기한다"]
-m1_e21 = ["당신은 결국 트럭를 피하지 못했습니다. 이또한 운명이겠지요", 
+m1_e1 = ["바람 한점 없는 조용한 밤, 간만에 거하게 달린 당신은 휘청이는 몸을 이끌고 집으로 향하는 길이였다.  신호등이 초록불로 바뀌자 저벅저벅 길을 건너는데 건너편에서 트럭 경적이 울린다. 얼핏보기에도 10톤은 족히 넘어보이는 트럭이 경적을 울리며 달려오는데 멈출 기미가 보이질 않는다. 어떻게 해야할까?", 
+       "1. 몸을 날려 피한다.", 
+       "2. 운명을 받아들이고 포기한다."]
+m1_e211 = [" 몸을 있는 힘껏 날려보았지만 사람이 아무리 날래도 달려오는 트럭을 피할 수는 없었다.  마치 야구 배트에 맞은 야구공처럼 20m가량을 날라갔다. 온몸의 뼈가 으스러진 것만 같다. 살려달라고 소리치고 싶었지만 부러진 갈비뼈가 폐를 관통해 목소리가 나오지 않는다. 찢어진 이마에서 흐른 피가 시야를 가리더니 이내 눈앞이 깜깜해졌다", 
            "계속"]
-m1_e22 = ["당신은 가까스로 트럭를 피했습니다. 뛰어난 반사신경, 운동실력, 그리고 운이 결합된 결과일 것입니다", 
-          "1. 트럭 기사에게 따진다", 
-          "2. 운이 좋았다 생각하고 집으로 돌아간다"]
+m1_e212 = ["본능적으로 몸을 날려 트럭을 피했다. '내가 이정도의 반사신경이 있었나? 알코올이 날 각성시키기라도 한건가?'. 독백도 잠시 기사가 고래고래 소리치기 시작한다. '빨간 불에 차도로 걸어나오면 죽여달라고 환장한거야?? 멀쩡한 사람 인생 망치려고 작정했어???'  분명 초록불을 보고 건넜는데 저 기사가 무슨 헛소리를 하는 거지 싶던 것도 잠시 횡단보도의 초록불이 깜빡이기 시작한다. 당황스러움과 함께 취기가 날라갔다. '지금 초록불이 깜빡인다는건 아까 내가 본 초록불은..'  기사님께 재차 사과를 드리고 적당히 마셔야겠다는 생각과 함께 집으로 향했다.", 
+          "(히든 엔딩 - 과음은 위험해) "]
 ep3 = ["서브이벤트 입니다 줄바꿈 테스트 중 줄바 꿈 이이이잉 앗쌀라마라이꿍", 
        "1. ㅁㄴㅇㄹ", 
        "2. ㅁㄴㅇㄹ", 
@@ -46,10 +47,14 @@ def main_e1():
        global day, step, choice, choice2
        if step == 0:
               Text.output(m1_e1, cg[0])
+              
        elif step == 1:
               if choice == 1:
-                     Text.output(m1_e21)
                      choice2 = 1
+                     if per(95, 1):
+                            Text.output(m1_e211)
+                     else:
+                            Text.output(m1_e212)
               elif choice == 2:
                      Text.output(m1_e22)
                      choice2 = 2
@@ -92,7 +97,7 @@ class Text:
                             # 문자열 길이 측정위해 렌더
                             text_surface = font.render(' '.join(current_line), True, color1)
                             # 길이 측정 후 라인스에 넣기
-                            if text_surface.get_width() > 450:
+                            if text_surface.get_width() > 440:
                                    current_line.pop()
                                    lines.append(' '.join(current_line))
                                    current_line = [word]
@@ -107,11 +112,11 @@ class Text:
                             textrect = textobj.get_rect()
                             # x좌표, y좌표로 위치 잡음 y좌표는 for문이 반복할 때 마다 높이만큼 증가
                             if ind == 0:
-                                   textrect.topleft = (50, 80 + i * font.get_height())
+                                   textrect.topleft = (20, 80 + i * font.get_height())
                                    if img != None and i == len(lines)-1:
                                           screen.blit(img, (120, 140 + i * font.get_height()))
                             else:
-                                   textrect.topleft = (50, 720 - ((len(episode) - ind) * 40 + 50))
+                                   textrect.topleft = (20, 720 - ((len(episode) - ind) * 40 + 50))
                             # 화면에 출력
                             screen.blit(textobj, textrect)
 
@@ -128,8 +133,18 @@ class Text:
 def highlight(rec):
       pg.draw.rect(screen, color1, rec, 2)
 
-def per(r):
-       return random.randint(1, 100) >= r      
+def per(r, seed):
+       global pers, perb
+       if pers == seed:
+              return perb
+       else:
+              pers = seed
+              perb = random.randint(1, 100) <= r
+              return perb
+       
+
+
+
     
 def next():
        global day, step
